1c136d74278b09531ff384a7a75c7e1f6bd6e650
it.chhaya <it.chhaya@gmail.com>
2024-12-06 19:08:17 +0700

    template send

--- src/main/java/co/istad/email/config/MailServerConfig.java
+++ src/main/java/co/istad/email/config/MailServerConfig.java
@@ -9,28 +9,38 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Configuration
 @RequiredArgsConstructor
 public class MailServerConfig {
 
     private final JavaMailSender javaMailSender;
+    private Context context;
+    private final TemplateEngine templateEngine;
 
     @Value("${spring.mail.username}")
     private String adminMail;
 
     public void send(MailRequest<?> mailRequest) {
         // TODO
+
+        context = new Context();
+        context.setVariable("data", mailRequest.data());
+
+        String proceedTemplate = templateEngine.process(mailRequest.template(), context);
+
         // 1. Create Message Object
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         // 2. Set Message Object Information
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
         try {
+            mimeMessageHelper.setText(proceedTemplate, true);
             mimeMessageHelper.setTo(mailRequest.to());
             mimeMessageHelper.setFrom(adminMail);
             mimeMessageHelper.setCc(mailRequest.cc());
             mimeMessageHelper.setSubject(mailRequest.subject());
-            mimeMessageHelper.setText(mailRequest.template());
             javaMailSender.send(mimeMessage);
         } catch (MessagingException e) {
             System.out.println("Error send mail: " + e.getMessage());
